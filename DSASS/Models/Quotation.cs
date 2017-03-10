900ecfd5305d54916a7fbe1b8e3a728a23b4358e
MandisoNgwenya <mandiso04@yahoo.com>
2017-03-10 13:51:45 +0200

    update test

--- DSASS/Models/Quotation.cs
+++ DSASS/Models/Quotation.cs
@@ -10,5 +10,9 @@ namespace DSASS.Models
         public int QID { get; set; }
         public int JobCardID { get; set; }
         public string customerNumber { get; set; }// ref from booking table
+        public string FaultDescription { get; set; }
+        public double Deposite { get; set; }
+        public double Balance { get; set; }
+        public double total { get; set; }
     }
 }
